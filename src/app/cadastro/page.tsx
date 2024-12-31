92574a2424fffc63b807f5177dbe129c3b011232
mikael-espinola <mikaelespinolaa@gmail.com>
2024-12-31 11:41:20 -0300

    :sparkles: feat: componentização do formulário de registro e aplicação de aria-label

--- src/app/cadastro/page.tsx
+++ src/app/cadastro/page.tsx
@@ -1,50 +1,18 @@
-"use client";
 import React, { FormEvent } from "react";
-import { Box, Button, Container, RegisterField } from "./style";
-import Input from "@/components/input/Input";
-import { useRouter } from "next/navigation";
+import { Container, Title } from "./style";
+import CadastroForm from "@/components/cadastroForm/CadastroForm";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "LanFLow - Cadastro",
+  description: "Página para cadastro de usuários",
+};
 
 const Cadastro = () => {
-  const router = useRouter();
-
-  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-  };
   return (
     <Container>
-      <RegisterField onSubmit={handleSubmit}>
-        <Input
-          label="Name"
-          id="name"
-          placeholder="Digite seu nome"
-          type="text"
-        />
-        <Input
-          id="email"
-          type="email"
-          label="E-mail"
-          placeholder="Digite seu e-mail  "
-        />
-        <Input
-          id="emailValidation"
-          type="email"
-          label="Confirme o e-mail"
-          placeholder="Digite seu e-mail  "
-        />
-        <Box>
-          <Input id="data-nascimento" type="date" label="Data de Nascimento" />
-          <Input
-            id="pass"
-            type="password"
-            label="Senha"
-            placeholder="Digite sua senha"
-          />
-        </Box>
-        <Button type="submit">Salvar</Button>
-        <Button type="button" onClick={() => router.push("/")}>
-          Voltar
-        </Button>
-      </RegisterField>
+      <Title aria-label="Cadastro de usuário">Cadastro de Usuário</Title>
+      <CadastroForm />
     </Container>
   );
 };
--- src/app/cadastro/style.tsx
+++ src/app/cadastro/style.tsx
@@ -3,29 +3,15 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   display: flex;
-  justify-content: center;
-`;
-export const RegisterField = styled.form`
-  margin-top: 1rem;
-  display: flex;
+  background-color: #cacaca;
+  min-height: 100vh;
   flex-direction: column;
-  width: 40rem;
 `;
 
-export const Button = styled.button`
-  border: none;
-  background-color: aqua;
-  padding: 0.4rem;
-  border-radius: 6px;
-  margin-bottom: 0.3rem;
-  cursor: pointer;
-
-  &:disabled {
-    cursor: not-allowed;
-  }
-`;
-
-export const Box = styled.div`
-  display: flex;
-  gap: 0.5rem;
+export const Title = styled.h3`
+  padding: 0.3rem;
+  cursor: default;
+  color: #ff6f00;
+  font-weight: 700;
+  font-size: 1.875rem;
 `;
